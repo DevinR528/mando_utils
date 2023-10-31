fe0026b063b1e7433f9b54a1c819ea38ce86dec2
Devin Ragotzy <devin.ragotzy@gmail.com>
2023-10-31 15:22:04 -0400

    start stuff

---
+++ file_ideas.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+
+struct policy {};
+struct str {};
+struct write {};
+
+int main(int argc, char const *argv[]) {
+    // How the hell do I make a test for this...
+
+    return 0;
+}
+
